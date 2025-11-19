07f1efe7545985d098bda4d5df88f6f673e4d906
hungtqph42816 <hungtqph42816@fpt.edu.vn>
2025-11-19 21:22:55 +0700

    brand edit and create

---
+++ resources/views/admin/brand/create.blade.php
@@ -0,0 +1,10 @@
+
+
+@extends('layouts.admin')
+@section('title', 'Thêm Nhân Brand')
+
+@section('content')
+<div class="form-container">
+    
+</div>
+@endsection
---
+++ resources/views/admin/brand/edit.blade.php
@@ -0,0 +1,8 @@
+{{-- File: resources/views/admin/staff/edit.blade.php --}}
+
+@extends('layouts.admin')
+@section('title', 'Sửa Thông Tin Brand')
+
+@section('content')
+
+@endsection
